ca2b76901ec40ccfafd0aaf1d22b288290be1f31
shaziya78 <shaikhshaziya2828@gmail.com>
2025-07-12 23:58:17 +0530

    url

--- src/constant.js
+++ src/constant.js
@@ -8,6 +8,7 @@ export const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://cyber-sapient-technologies-assessme-jade.vercel.app",
+    "guileless-meringue-086640.netlify.app"
   ],
   credentials: true,
 };
