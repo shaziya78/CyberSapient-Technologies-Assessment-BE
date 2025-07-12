539db265c41fe97a2a2997e58854f771e41a9dbe
shaziya78 <shaikhshaziya2828@gmail.com>
2025-07-12 23:10:37 +0530

    origin

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,10 @@
 export const cookiesOptions = {
-    httpOnly: true,
-    secure: true, 
-    sameSite: 'Strict',
-  }
+  httpOnly: true,
+  secure: true,
+  sameSite: "Strict",
+};
 
-export const corsOptions = { origin: "*", credentials: true }
+export const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
